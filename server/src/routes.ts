5ca43a62b37574e6a05b0c2ce00ebbaa195c22a9
riordan.santos <riordan3607@gmail.com>
2023-01-19 11:42:35 -0300

    sumarry route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -119,4 +119,30 @@ export async function appRoutes(app: FastifyInstance) {
       });
     }
   });
+
+  app.get("/summary", async () => {
+  const summary = await prisma.$queryRaw`
+    SELECT 
+      D.id, 
+      D.date, 
+      (
+        SELECT 
+          cast(count(*) as float) 
+        FROM day_habits DH 
+        WHERE Dh.day_id = D.id
+      ) as completed,
+      (
+        SELECT 
+          cast(count(*) as float) 
+        FROM habit_week_days WHD
+        JOIN habits H
+          ON H.id = HWD.habit_id
+        WHERE
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+      ) as amout
+    FROM days D
+    `;
+    return summary;
+  });
 }
